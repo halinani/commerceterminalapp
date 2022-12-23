f008f26669a14de167d4503e5a86c06328a0b332
Halinani Simwela <halinanivsimwela@gmail.com>
2022-12-23 15:58:20 +0200

    Item model done

---
+++ bin/item.dart
@@ -0,0 +1,12 @@
+import 'product.dart';
+
+class Item {
+  final Product product;
+  final int quantity;
+  final int price;
+  Item({
+    required this.product,
+    required this.quantity,
+    required this.price,
+  });
+}
