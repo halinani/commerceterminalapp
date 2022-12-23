0defbe6d34bfade477ec220bc2b54cc25ec36a59
Halinani Simwela <halinanivsimwela@gmail.com>
2022-12-23 15:48:58 +0200

    Start of Commerce Project

---
+++ bin/commerceterminalapp.dart
@@ -0,0 +1,5 @@
+import 'package:commerceterminalapp/commerceterminalapp.dart' as commerceterminalapp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${commerceterminalapp.calculate()}!');
+}
